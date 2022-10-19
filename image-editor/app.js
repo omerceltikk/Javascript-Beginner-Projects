663eda06b3e148f05a7226dc1dcba6e674c97c60
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2022-10-19 20:34:19 +0300

    image editor filter, rotate and reset buttons are completed

--- image-editor/app.js
+++ image-editor/app.js
@@ -3,6 +3,8 @@ let inputTitle = document.querySelector(".filter-info .title")
 let inputValue = document.querySelector(".filter-info .value")
 let sliderInput = document.querySelector(".slider input")
 let prevImage = document.querySelector(".preview-img img")
+let optRotate = document.querySelectorAll(".rotate button")
+let resetBtn = document.querySelector(".resetBtn")
 
 console.log(prevImage.style.filter)
 filterBtn.addEventListener("click", (e) => {
@@ -14,9 +16,50 @@ filterBtn.addEventListener("click", (e) => {
 sliderInput.addEventListener("mouseup", (e) => {
     inputValue.innerText = `${Math.floor(e.target.value)}%`
     if(inputTitle.innerText == "Brightness"){
-        let calc = `${e.target.value}%`
-        console.log(calc)
-        prevImage.style.filter="Brightness(e.target.value)"
+        let calc = `Brightness(${e.target.value}%)`
+        prevImage.style.filter=calc
+    }
+    if(inputTitle.innerText == "Saturation"){
+        let calc = `Saturate(${e.target.value}%)`
+        prevImage.style.filter=calc
+    }
+    if(inputTitle.innerText == "Inversion"){
+        let calc = `Invert(${e.target.value}%)`
+        prevImage.style.filter=calc
+    }
+    if(inputTitle.innerText == "Grayscale"){
+        let calc = `Grayscale(${e.target.value}%)`
+        prevImage.style.filter=calc
     }
 })
 //
+optRotate.forEach(element => {
+    element.addEventListener("click", (element)=>{
+        if (element.target.classList.value || element.target.firstChild.classList.value == "fa-solid fa-rotate-right") {
+            prevImage.style.transform += "rotate(90deg)"
+        }
+        if (element.target.classList.value || element.target.firstChild.classList.value == "fa-solid fa-rotate-left") {
+            prevImage.style.transform += "rotate(-90deg)"
+        }
+        if (element.target.classList.value || element.target.firstChild.classList.value == "bx bx-reflect-vertical") {
+            prevImage.style.transform += "scaleX(-1)"
+        }
+        if (element.target.classList.value || element.target.firstChild.classList.value == "bx bx-reflect-horizontal") {
+            prevImage.style.transform += "scaleY(-1)"
+        }
+        
+    })
+    
+});
+
+resetBtn.addEventListener("click", ()=> {
+    prevImage.style.filter = "Brightness(100%)"
+    prevImage.style.filter = "Saturate(100%)"
+    prevImage.style.filter = "Invert(100%)"
+    prevImage.style.filter = "Grayscale(100%)"
+    prevImage.style.transform="rotate(0deg)"
+    inputValue.innerText ="100%"
+    sliderInput.value="100"
+});
+
+////save and download image 
