df6c99a47d8966402068667db9585bc163a49298
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2022-10-21 01:25:25 +0300

    upload and download options completed

--- image-editor/app.js
+++ image-editor/app.js
@@ -5,6 +5,21 @@ let sliderInput = document.querySelector(".slider input")
 let prevImage = document.querySelector(".preview-img img")
 let optRotate = document.querySelectorAll(".rotate button")
 let resetBtn = document.querySelector(".resetBtn")
+let choseImg = document.querySelector(".choose-image")
+let fileInput = document.querySelector(".file-input")
+let saveBtn = document.querySelector(".save-btn")
+
+
+let changeImage = function (){
+    let file= fileInput.files[0];
+    if(!file) return;
+    prevImage.src=URL.createObjectURL(file); 
+}
+changeImage();
+fileInput.addEventListener("change", changeImage)
+choseImg.addEventListener("click",()=> fileInput.click());
+prevImage.addEventListener("click", ()=> fileInput.click())
+
 
 console.log(prevImage.style.filter)
 filterBtn.addEventListener("click", (e) => {
@@ -13,7 +28,7 @@ filterBtn.addEventListener("click", (e) => {
     inputValue.innerText="100%"
 });
 
-sliderInput.addEventListener("mouseup", (e) => {
+let updateFilter = sliderInput.addEventListener("mouseup", (e) => {
     inputValue.innerText = `${Math.floor(e.target.value)}%`
     if(inputTitle.innerText == "Brightness"){
         let calc = `Brightness(${e.target.value}%)`
@@ -56,10 +71,57 @@ resetBtn.addEventListener("click", ()=> {
     prevImage.style.filter = "Brightness(100%)"
     prevImage.style.filter = "Saturate(100%)"
     prevImage.style.filter = "Invert(100%)"
-    prevImage.style.filter = "Grayscale(100%)"
+    prevImage.style.filter = "Grayscale(0%)"
     prevImage.style.transform="rotate(0deg)"
     inputValue.innerText ="100%"
     sliderInput.value="100"
 });
-
 ////save and download image 
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+
+let brightnessImage = function(){
+    if(inputTitle.innerText == "Brightness"){
+        ctx.filter=`Brightness(${sliderInput.value}%)`
+    }
+}
+let saturateImage = function(){
+    if(inputTitle.innerText == "Saturation"){
+        ctx.filter=`saturate(${sliderInput.value}%)`
+}
+    }
+let invertImage = function(){
+    if (inputTitle.innerText == "Inversion") {
+        ctx.filter=`invert(${sliderInput.value}%)`
+    }
+}
+let grayImage = function(){
+    if (inputTitle.innerText == "Grayscale") {
+        ctx.filter=`grayscale(${sliderInput.value}%)`
+    }
+}
+
+let setImgScale = function(){
+    if(prevImage.style.transform =="-1"){
+        ctx.scale(-1,1)
+    }
+}
+let saveImage = () => {
+    canvas.width=prevImage.naturalWidth;
+    canvas.height=prevImage.naturalHeight;
+    brightnessImage();
+    saturateImage();
+    invertImage();
+    grayImage();
+    ctx.translate(canvas.width/2,canvas.height/2)
+    setImgScale();
+    ctx.drawImage(prevImage, -canvas.width/2,-canvas.height/2, canvas.width, canvas.height);
+    document.body.appendChild(canvas);
+    
+    let link = document.createElement("a");
+    link.download = "image.jpg";
+    link.href=canvas.toDataURL()
+    link.click();
+}
+
+saveBtn.addEventListener("click", saveImage)
